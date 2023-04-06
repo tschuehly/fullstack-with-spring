3dc69d5a828dd8cab26d34074e1256cd158d0c65
Thomas Schuehly <thomas.schuehly@outlook.com>
2023-04-06 22:29:34 +0200

    generic sidebar with enum

--- src/main/kotlin/de/tschuehly/rapidfullstackdemo/web/layout/sidebar/SidebarViewComponent.kt
+++ src/main/kotlin/de/tschuehly/rapidfullstackdemo/web/layout/sidebar/SidebarViewComponent.kt
@@ -8,10 +8,16 @@ import de.tschuehly.thymeleafviewcomponent.toProperty
 @ViewComponent
 class SidebarViewComponent {
     fun render(activeTab: ActiveTab) = ViewContext(
-        "activeTab" toProperty activeTab
+        "activeTab" toProperty activeTab,
     )
 
-    enum class ActiveTab {
-        Dashboard, BlankPage, Tables, Forms, TabbedContent, Calendar
+    enum class ActiveTab(val displayName: String, val icon: String, val href: String) {
+        Dashboard("Dashboard", "fa-tachometer-alt", "/"),
+        BlankPage("Blank Page", "fa-sticky-note", "/blank-page"),
+        Tables("Tables", "fa-table", "/tables"),
+        Forms("Forms", "fa-align-left", "/forms"),
+        TabbedContent("Tabbed Content", "fa-tablet-alt", "/tabbed"),
+        Calendar("Calendar", "fa-calendar", "/calendar")
+
     }
 }
