772530665571fcd5ce22f5d935efc99ebeba6b2b
Thomas Schuehly <thomas.schuehly@outlook.com>
2023-06-17 15:18:58 +0200

    update

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,7 @@
 rootProject.name = "rapid-fullstack-demo"
+
+includeBuild("E:\\Programmieren\\supabase-security-spring-boot-starter"){
+    dependencySubstitution {
+        substitute(module("de.tschuehly:supabase-security-spring-boot-starter")).using(project(":"))
+    }
+}
