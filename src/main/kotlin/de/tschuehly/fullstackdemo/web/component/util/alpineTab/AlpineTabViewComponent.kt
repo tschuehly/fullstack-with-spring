8ee5af85d367f7323058c00d5086b9d91ac6b060
Thomas Schuehly <tschuehly@outlook.com>
2024-03-02 17:00:49 +0100

    rename context

--- src/main/kotlin/de/tschuehly/fullstackdemo/web/component/util/alpineTab/AlpineTabViewComponent.kt
+++ src/main/kotlin/de/tschuehly/fullstackdemo/web/component/util/alpineTab/AlpineTabViewComponent.kt
@@ -5,16 +5,17 @@ import de.tschuehly.spring.viewcomponent.thymeleaf.ViewContext
 
 @ViewComponent
 class AlpineTabViewComponent {
-    fun render(tabs: List<Tab>) = AlpineTabCtx(
-        tabs.map { it.tabHeader },
-        tabs.map { it.tabContent }
-    )
-    class Tab(
-        val tabHeader: String,
-        val tabContent: String
-    )
-    data class AlpineTabCtx(
+    fun render(tabs: List<Tab>): AlpineTabContext {
+        return AlpineTabContext(
+            tabs.map { it.tabHeader },
+            tabs.map { it.tabContent }
+        )
+    }
+
+    class Tab( val tabHeader: String, val tabContent: String )
+
+    data class AlpineTabContext(
         val headerList: List<String>,
         val contentList: List<String>
-    ):ViewContext
+    ) : ViewContext
 }
